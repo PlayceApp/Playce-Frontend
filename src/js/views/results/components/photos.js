99bb1ed24b98c3d12b3b70a919a8bfd4aeac2f9e
spoorthyv <sckortyman@gmail.com>
2018-06-05 17:06:23 -0700

    Center photos section

--- src/js/views/results/components/photos.js
+++ src/js/views/results/components/photos.js
@@ -8,7 +8,7 @@ const Container = styled.div`
    flex-direction: column;
    animation: ${animation.fadeIn} 0.3s;
    max-width: 48em;
-   margin: 16px auto 22px 100px;
+   margin: 16px auto 22px auto;
 `;
 
 const Header = styled.h2`
